6c99848ece8767cbf4d582b8a02da5c46fc16378
Norfend <fenker@protonmail.com>
2024-11-27 12:09:32 +0100

    Added signup page

--- semestral/component/header.php
+++ semestral/component/header.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cooking Recipes</title>
-    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="../style/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
 <header class="header">
@@ -14,10 +17,10 @@
         </div>
         <nav class="nav" aria-label="Main navigation">
             <ul class="nav-links">
-                <li><a href="#home">Home</a></li>
+                <li><a href="../index.php">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#recipes">Recipes</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="../page/signup.php">Contact</a></li> <!--temporary link!!!!!!!-->
             </ul>
         </nav>
     </div>
---
+++ semestral/page/signup.php
@@ -0,0 +1,25 @@
+<?php
+require_once '../component/header.php';?>
+
+<main class="main-content">
+    <form action="signup.php" method="POST" enctype="multipart/form-data" class="signup-form">
+        <h1>Create an Account</h1>
+        <label for="first_name">First Name:</label>
+        <input type="text" id="first_name" name="first_name" required>
+        <label for="last_name">Last Name:</label>
+        <input type="text" id="last_name" name="last_name" required>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <label for="confirm_password">Confirm Password:</label>
+        <input type="password" id="confirm_password" name="confirm_password" required>
+        <label for="avatar">Avatar (Optional):</label>
+        <input type="file" id="avatar" name="avatar" accept="image/*">
+        <button type="submit" name="submit">Sign Up</button>
+    </form>
+</main>
+
+<?php include '../component/footer.php';?>
